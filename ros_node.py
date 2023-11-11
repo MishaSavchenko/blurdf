8248ebbd6162b245a5ebdf3f3054aef64c15e4eb
Misha Savchenko <savchenkmisha@gmail.com>
2023-11-11 15:53:18 -0600

    updated joint constraints

--- ros_node.py
+++ ros_node.py
@@ -41,7 +41,7 @@ def load_geometry(geom):
     
 def load_visual(visual):
     if visual is None: 
-        bpy.ops.object.empty_add() 
+        bpy.ops.object.empty_add(radius=0.25) 
     else:
         load_geometry(visual.geometry)
         
@@ -73,12 +73,25 @@ def main():
         t = mathutils.Matrix.Translation(joint.origin.xyz).translation
         T.translation = t
         
-
-        links[joint.child].matrix_world = T
         links[joint.child].parent = links[joint.parent]
+        links[joint.child].matrix_local = T
         
+        bpy.ops.object.empty_add(radius=0.01)
+        
+        joint_frame = bpy.context.object
+        joint_frame.name = joint.name
+        joint_frame.parent = links[joint.parent]
+        joint_frame.matrix_local = T 
+
+#        bpy.context.view_layer.update()
+#        if(True):
+
+#        if(joint.type == "fixed"):
+
         lim_loc = links[joint.child].constraints.new("LIMIT_LOCATION")
-        lim_loc.owner_space = "LOCAL"
+        lim_loc.owner_space = "CUSTOM"
+        lim_loc.space_object = joint_frame
+
         lim_loc.use_min_x = True
         lim_loc.use_min_y = True
         lim_loc.use_min_z = True
@@ -86,19 +99,35 @@ def main():
         lim_loc.use_max_x = True
         lim_loc.use_max_y = True
         lim_loc.use_max_z = True
+
         lim_rot = links[joint.child].constraints.new("LIMIT_ROTATION")
+        lim_rot.owner_space = "CUSTOM"
+        lim_rot.space_object = joint_frame
 
-
-        links[joint.child].matrix_world = T
-        links[joint.child].parent = links[joint.parent]
-#        links[joint.child].matrix_local = T
-
-#        print(links[joint.child].constraints)
-#        print(links[joint.child].matrix_world)
-#        print(links[joint.child].matrix_local)
-#        print(links[joint.child].matrix_parent_inverse)
-#        print(links[joint.child].matrix_basis)
-
+        lim_rot.use_limit_x = True
+        lim_rot.use_limit_y = True
+        lim_rot.use_limit_z = True
+        
+        if joint.type == "revolute":
+    
+            print(joint.limit)
+            low = joint.limit.lower
+            high = joint.limit.upper
+            
+            norm_axis = np.abs(joint.axis / np.linalg.norm(joint.axis))
+            if norm_axis[0]: 
+                print("x axis") 
+                lim_rot.min_x, lim_rot.max_x = low, high
+            elif norm_axis[1]:
+                print("y axis") 
+                lim_rot.min_y, lim_rot.max_y = low, high
+            elif norm_axis[2]:
+                print("z axis") 
+                lim_rot.min_z, lim_rot.max_z = low, high
+            else: 
+                lim_rot.use_limit_x = False
+                lim_rot.use_limit_y = False
+                lim_rot.use_limit_z = False
 
 
 if __name__ == "__main__":
